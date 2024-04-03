6befc7253d7df6937dc0c44be8bb76c55587cfe7
SinghShubham1308 <shubhusingh1308@gmail.com>
2024-04-03 12:52:42 +0530

    functionalities added using scripts.js

--- NavBar/scripts.js
+++ NavBar/scripts.js
@@ -1,7 +1,7 @@
 const toggleBtn = document.querySelector(".nav-toggle");
 let links = document.querySelector(".links");
-var classList = el.classList;
-console.log(classList.contains("show-links"));
+
+console.log(links.classList.contains("show-links"));
 toggleBtn.addEventListener("click", function () {
 //   if (classList.contains("show-links")) {
 //     links.classList.remove("show-links");
