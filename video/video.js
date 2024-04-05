3353328cfc8adf742c665948d49b11ae23d5e23e
SinghShubham1308 <shubhusingh1308@gmail.com>
2024-04-05 15:02:19 +0530

    added js to the project

---
+++ video/video.js
@@ -0,0 +1,16 @@
+// Get the video element
+const video = document.querySelector(".video-container");
+
+// Get the switch button
+const switchBtn = document.querySelector(".switch-btn");
+
+// Add click event listener to the switch button
+switchBtn.addEventListener("click", function () {
+  switchBtn.classList.toggle("slide");
+  if (video.paused) {
+    // If paused, play the video
+    video.play();
+  } else {
+    video.pause();
+  }
+});
